8f30b225ee6c1c14cc9cc00070c738830fb4532b
Milly <liuhongmein@gmail.com>
2023-02-14 13:52:31 -0500

    qr code

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="app">
+      <img src="./src/images/image-qr-code.png" alt="qr code" />
+      <div className="text-wrapper">
+        <p className="head">
+          Improve your front-end skills by building projects
+        </p>
+        <p className="desc">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level !
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
